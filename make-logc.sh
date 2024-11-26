e10420a291e554eeade3e35d1ac2c36822657fae
arjn <arjunal9000@gmail.com>
2024-11-26 22:34:07 +0530

    Create make-logc.sh

---
+++ make-logc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install required system packages
+sudo apt update
+sudo apt install -y python3-pip python3-venv tcpdump
+
+cd ./
+# Create virtual environment
+python3 -m venv venv
+
+# Activate virtual environment
+source ./venv/bin/activate
+
+# Install required Python packages
+pip install watchdog
+pip install python-dotenv
+pip install psutil
+
+# Create log directories
+mkdir -p logs
+mkdir -p captures
+
+# Set permissions
+sudo chmod 755 ./
+sudo chown -R $USER:$USER ./
+
+echo "Virtual environment setup complete in $PWD"
+echo "To activate the virtual environment, use: source $PWD/venv/bin/activate"
